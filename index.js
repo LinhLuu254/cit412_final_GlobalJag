199f695d3051983dd410fe55f50e6444215b15bb
Linh Luu <linluu@iu.edu>
2024-02-22 21:44:37 +0000

    version 2

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ exports.generateThumbData = async (file, context) => {
 }
 
 
-
 // Entry point function
 const generateThumbnails = async (file, context) => {
 
@@ -33,7 +32,65 @@ const generateThumbnails = async (file, context) => {
     let fileExtension = '';
     let validFile = false;
 
+    if (gcsFile.contentType === 'image/jpeg') {
+        console.log('This is a JPG file');
+        fileExtension = 'jpg';
+        validFile = true;
+    
+      } else if (gcsFile.contentType === 'image/png'){
+        console.log('This is a PNG file');
+        fileExtension = 'png';
+        validFile = true;
+    
+      } else {
+        console.log('This is not a valid file');
+      }
+
+    if (validFile) {
+    // Create a new filename for the 'final' version of the image file
+
+    const finalFileName = `${gcsFile.generation}.${fileExtension}`
+
+    // Create a working directory on the VM that runs our GCF to download the original file
+    // The value of this variable will be something like 'tmp/thumbs'
+    const workingDir = path.join(os.tmpdir(), 'thumb');
+
+    // Create a variable that holds the path to the 'local' version of the file
+
+    const tempFilePath = path.join(workingDir, finalFileName)
+
+    // Wait until the working directory is ready
+    await fs.ensureDir(workingDir);
+
+    // Download the original file to the path on the 'local' VM
+    await sourceBucket.file(gcsFile.name).download({
+        destination: tempFilePath
+    });
+
+    // Upload our local version of the file to the final images bucket
+    await finalBucket.upload(tempFilePath);
+
+    // Create a name for the thumbnail image
+    const thumbName = `thumb@64_${finalFileName}`;
+
+    // Create a path where we will store the thumbnail image locally
+    const thumbPath = path.join(workingDir, thumbName);
+
+    // Use the sharp library to generate the thumbnail image and save it to the thumbPath
+    // Then upload the thumbnail to the thumbnailsBucket in cloud storage
+    await sharp(tempFilePath)
+    .resize(64)
+    .withMetadata()
+    .toFile(thumbPath)
+    .then( async () => {
+        await thumbnailsBucket.upload(thumbPath)
+    });
+
+    // Delete the temp working directory and its files from the GCF's VM
+    await fs.remove(workingDir);
+    } 
+
     // DELETE the original file uploaded to the "Uploads" bucket
     await sourceBucket.file(gcsFile.name).delete();
     console.log(`Deleted uploaded file: ${gcsFile.name}`)
-}
+};
